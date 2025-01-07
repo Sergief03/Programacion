f54e8094530868c4e4983cd6bef424dce7b9d16f
Sergief03 <sergioandrespaco@gmail.com>
2025-01-07 09:28:46 +0100

    Prueba

--- Unidad4/src/Ejercicio22.java
+++ Unidad4/src/Ejercicio22.java
@@ -9,28 +9,26 @@ public class Ejercicio22 {
             System.out.println("Introduce una palabra: ");
             palabras[i]=new Scanner(System.in).nextLine();
         }
-        for (int i=0;i<numero;i++){
-            if (i==0){
-                System.out.print("{");
-            } else if (i==numero-1) {
-                System.out.print(palabras[i]+"}\n");
-            }else {
-                System.out.print(palabras[i]+", ");
+        System.out.print("Palabras introducidas: {");
+        for (int i = 0; i < numero; i++) {
+            System.out.print(palabras[i]);
+            if (i < numero - 1) {
+                System.out.print(", ");
             }
         }
-        System.out.println("Las palabras que contiene a son: ");
-        for (int i=0;i<numero;i++){
-            if (palabras[i].contains("a")){
-                if (i==0){
-                    System.out.print("{");
-                } else if (i==numero) {
-                    System.out.print(palabras[i]+"}");
-                }else {
-                    System.out.print(palabras[i]+", ");
+        System.out.println("}");
+
+        System.out.print("Las palabras que contienen la letra 'a' son: {");
+        boolean primeraPalabra = true;
+        for (String palabra : palabras) {
+            if (palabra.contains("a")) {
+                if (!primeraPalabra) {
+                    System.out.print(", ");
                 }
+                System.out.print(palabra);
+                primeraPalabra = false;
             }
         }
-
-
+        System.out.println("}");
     }
 }
